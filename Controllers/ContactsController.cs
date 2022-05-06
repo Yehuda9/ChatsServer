c4baf56720052df2fd6808e25c46805b7b648a09
yehuda <yehuda7701@gmail.com>
2022-05-06 11:35:35 +0300

    basic action in contacts controller

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 
 [Route("api/[controller]")]
 [ApiController]
 public class ContactsController : ControllerBase
 {
-    private static List<Contact> contacts = new List<Contact>();
-
+    private static List<Contact> contactsList = new List<Contact>();
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        return JsonSerializer.Serialize(contactsList);
+    }
+    [HttpPost]
+    public IActionResult Post(string id,string name,string server)
+    {
+        contactsList.Add(new Contact(id,name,server));
+        return StatusCode(200);
+    }
 }
 
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,8 +1,23 @@
-﻿public class Contact
+﻿[Serializable]
+public class Contact
 
 {
-    string? name { get; set; }
-    List<Chat> chats { get; set; }
+    public Contact(string id,string name,string server)
+    {
+        this.id = id;
+        this.name = name;
+        this.chats=new List<Chat>();
+        this.server = server;
+        this.last = "";
+        this.lastdate = new DateTime();
+    }
+    public string? id { get; set; }
+    public string? name { get; set; }
+    public string? server { get; set; }
+    public List<Chat> chats { get; set; }
+    public string last { get; set; }
+    public DateTime lastdate { get; set; }
+    
 
 }
 
